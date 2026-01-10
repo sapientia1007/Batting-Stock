026283d4f53292438feb3773c3301f19aab8ecff
sapientia1007 <179jh@naver.com>
2026-01-10 21:03:25 +0900

    init : Initial commit

---
+++ src/main/java/org/example/battingstock/BattingStockApplication.java
@@ -0,0 +1,13 @@
+package org.example.battingstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattingStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattingStockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/battingstock/BattingStockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.battingstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattingStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
